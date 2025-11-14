c38468aff4d535e91fd829dbf2f660fc8736a0dc
Anirudh-1810 <anirudhsharma73093@gmail.com>
2025-11-14 11:24:23 +0530

    h

--- brainbug-backend/src/routes/bugHistoryRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBugHistory } from "../controllers/bugHistoryController.js";
-
-const router = express.Router();
-
-router.get("/", getBugHistory);
-
-export default router;
-
--- brainbug-backend/src/services/mlService.js
+++
@@ -1,8 +0,0 @@
-export const runMLModel = async (code) => {
-    // TODO: Load actual ML model later
-    return {
-        bug_count: 2,
-        bugs: ["Unused variable", "Potential infinite loop"],
-        quality_score: 6.8
-    };
-};
